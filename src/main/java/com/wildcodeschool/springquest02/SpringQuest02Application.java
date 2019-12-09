7f0a7af837c5306cbb2806d5e3f20acdb88d6ac1
marcelkamp1 <marcelkamp1@gmx.de>
2019-12-09 12:59:29 +0100

    Spring Challenge 02

---
+++ src/main/java/com/wildcodeschool/springquest02/SpringQuest02Application.java
@@ -0,0 +1,50 @@
+package com.wildcodeschool.springquest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+@SpringBootApplication
+public class SpringQuest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuest02Application.class, args);
+	}
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Doctor Who:"
+				+ "<ul><li><a href='/doctor/1'>Doctor 1</a></li>"
+				+ "<li><a href='/doctor/7'>Doctor 7</a></li>"
+				+ "<li><a href='/doctor/10'>Doctor 10</a></li>"
+				+ "<li><a href='/doctor/13'>Doctor 13</a></li></ul>";
+	}
+	
+	@RequestMapping("/doctor/{number}")
+	@ResponseBody
+	public String doc1(@PathVariable int number) {
+		if(number <= 0 || number > 13) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible to retrieve the incarnation " + number);
+		}
+		if(number >= 1 && number <= 8) {
+			throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+		}
+		if(number >= 9 && number <= 13) {
+			switch(number){
+				case 9: return "{\"number\": " + number + ", \"name\": \"Christopher Eccleston\"}";
+				case 10: return "{\"number\": " + number + ", \"name\": \"David Tennant\"}";
+				case 11: return "{\"number\": " + number + ", \"name\": \"Matt Smith\"}";
+				case 12: return "{\"number\": " + number + ", \"name\": \"Peter Capaldi\"}";
+				case 13: return "{\"number\": " + number + ", \"name\": \"Jodie Whittaker\"}";
+			}
+		}
+		return "Your number was " + number;
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/springquest02/SpringQuest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.springquest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuest02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
